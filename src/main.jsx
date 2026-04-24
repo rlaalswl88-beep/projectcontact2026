71f4e6bf3a27f6079314a142009541940079b3bf
rlaalswl88-beep <rlaalswl88@gmail.com>
2026-04-24 12:29:43 +0900

    수정

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/dongaProject">
+            <BrowserRouter>
                 <App />
             </BrowserRouter>
         </Provider>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/dongaProject/",
     plugins: [plugin()],
     server: {
         port: 51482,
